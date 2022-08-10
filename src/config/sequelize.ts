3b24fbac6dff58c88fb4dbfa03628ac44709eca4
Alek30 <alemank23k@gmail.com>
2022-08-10 07:00:16 -0300

    second commit

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,106 +1,57 @@
-// const {Sequelize} = require('sequelize');
-import { Sequelize } from "sequelize";
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-// const setupModels = require('../db/models/index');
 import setupModels from "../db";
-const path = require("path");
-const fs = require("fs");
+const { Sequelize } = require("sequelize");
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+import { config } from "./config";
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-// const {config} = require('../config/config');
-// import { config } from "./config";
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/universalmusic`,
+        { logging: false, native: false }
+      );
+// const dbUrlLocal: string = postgres://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName};
 
-// const dbUrl: string = `postgres://${config.dbUser}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const url: string = ${config.dbUrl};
+
+// interface Options {
+//   dialect: any,
+//   logging: boolean
+//   dialectOptions: object
+// }
+
+// let options= {
+//   dialect: 'postgres', //elijo la db que voy a utilizar
+//   logging: config.isProd ? false : true,
+//   dialectOptions: {}
+//  }
+
+//  if(config.isProd) {
+//   options.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized:false
+//     }
+//   }
+//  }
 
 // const sequelize: Sequelize = new Sequelize(dbUrl, {
 //   dialect: "postgres",
 //   logging: true,
 // });
+// const sequelize = new Sequelize(url, options);
 
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/mypc`, {
-        logging: false,
-        native: false,
-      });
+// setupModels(sequelize);
+// sequelize.sync({ force: false });
 
-setupModels(sequelize);
-sequelize.sync({ force: false });
-
-export default sequelize;
-// module.exports = sequelize;;
-
-const basename = path.basename(__filename);
-
-const modelDefiners: any[] = [];
-//hola
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, "/models"))
-  .filter(
-    (file: any) =>
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      (file.slice(-3) === ".ts" || file.slice(-3) === ".js")
-  )
-  .forEach((file: any) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
-  });
-
-// Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach((model) => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [
-  entry[0][0].toUpperCase() + entry[0].slice(1),
-  entry[1],
-]);
-sequelize.models = Object.fromEntries(capsEntries);
-
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-
-const { User, Product, Order } = sequelize.models;
-
-// Aca vendrian las relaciones
-
-/* Un usuario tiene muchos procutos // Un producto pertenece a muchos usuarios */
-User.hasMany(Product, { sourceKey: "id", foreignKey: "user_id", as: "sell" });
-// console.log(sequelize.models)
-
-// User.hasMany(Product);
-Product.belongsTo(User);
-
-/* Un usuario tiene muchos pedidos // Un pedido pertenece a un usuario */
-User.hasMany(Order);
-Order.belongsTo(User);
-
-/* Un pedido tiene muchos procutos // Un producto pertenece a muchos pedidos */
-Order.hasMany(Product);
-Product.belongsTo(Order);
-
-module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
-};
+// // export default sequelize;
+// module.exports = sequelize
